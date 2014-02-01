72b476f012bd5729fa05de91c49b829adaf60f0c
Alexander Turiak <alex@hexbrain.com>
2014-02-01 19:56:37 +0200

    Implemented conflicts checker

--- app/code/community/Hackathon/MageMonitoring/Block/System/Overview/Read/Tabs/Rewrites.php
+++ app/code/community/Hackathon/MageMonitoring/Block/System/Overview/Read/Tabs/Rewrites.php
@@ -5,9 +5,36 @@ class Hackathon_MageMonitoring_Block_System_Overview_Read_Tabs_Rewrites
 {
     protected $_template = 'monitoring/rewrites.phtml';
 
-    public function getRewritesList()
+    /**
+     * Returns list of all rewrites in the system
+     *
+     * @return mixed
+     */
+    public function getRewritesInfo()
     {
-        return Mage::getModel('magemonitoring/rewrites')->getRewrites();
+        $rewrites = Mage::getModel('magemonitoring/rewrites')->getRewrites();
+
+        return $rewrites;
+    }
+
+    /**
+     * Return rewrite warning level
+     *
+     * @param array $rewriteInfo Rewrite info array
+     *
+     * @return string
+     */
+    public function getIconType($rewriteInfo)
+    {
+        if (isset($rewriteInfo['conflicts']) && count($rewriteInfo['conflicts']) > 1) {
+            return 'error';
+        }
+
+        if (isset($rewriteInfo['classes']) && count($rewriteInfo['classes']) > 1) {
+            return 'warning';
+        }
+
+        return 'info';
     }
 
 }
