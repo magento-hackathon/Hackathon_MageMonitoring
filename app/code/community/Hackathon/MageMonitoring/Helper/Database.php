73f5aa8452980ddee46c563f2d939305637b0925
Andreas Vogt <andreas.vogt@webvisum.de>
2014-05-10 18:18:57 +0200

    dding some first information getter on simple sql basis

--- app/code/community/Hackathon/MageMonitoring/Helper/Database.php
+++ app/code/community/Hackathon/MageMonitoring/Helper/Database.php
@@ -4,6 +4,9 @@ class Hackathon_MageMonitoring_Helper_Database extends Mage_Core_Helper_Abstract
 {
     private $_connection = null;
 
+    #private $_settings = array("have_innodb");
+    private $_settings = array();
+
     public function getConnection()
     {
         if (!$this->_connection) {
@@ -22,11 +25,52 @@ class Hackathon_MageMonitoring_Helper_Database extends Mage_Core_Helper_Abstract
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getMysqlServerInfo()
     {
-        return "ToDo";
+        /**
+         * Retrieve the read connection
+         */
+        $readConnection = $this->getConnection();
+
+        $query = 'SHOW VARIABLES LIKE "%version%"';
+
+        /**
+         * Execute the query and store the results in $results
+         */
+        $results = $readConnection->fetchAll($query);
+
+        foreach ($results as $_result){
+            if($_result['Variable_name'] == 'version'){
+                $result[] = array('label' => $_result['Variable_name'], 'value' => $_result['Value']);
+            }
+        }
+
+        return $result;
+    }
+
+    public function getMysqlServerStatus()
+    {
+        /**
+         * Retrieve the read connection
+         */
+        $readConnection = $this->getConnection();
+
+        $query = 'SHOW STATUS;';
+
+        /**
+         * Execute the query and store the results in $results
+         */
+        $results = $readConnection->fetchAll($query);
+
+        foreach ($results as $_result){
+            if($_result['Variable_name'] == 'version'){
+                $result[] = array('label' => $_result['Variable_name'], 'value' => $_result['Value']);
+            }
+        }
+
+        return $results;
     }
 
     public function getMysqlTunerOutput()
@@ -51,11 +95,30 @@ class Hackathon_MageMonitoring_Helper_Database extends Mage_Core_Helper_Abstract
 
     public function getMysqlServerSettingInformation()
     {
-        $_settings = array(
-            "char_set"
-        );
 
-        return "ToDo";
+        /**
+         * Retrieve the read connection
+         */
+        $readConnection = $this->getConnection();
+
+        $query = 'SHOW VARIABLES;';
+
+        /**
+         * Execute the query and store the results in $results
+         */
+        $results = $readConnection->fetchAll($query);
+
+        if (count($this->_settings) == 0) {
+            return $results;
+        }
+
+        foreach ($results as $_result){
+            if (in_array($_result['Variable_name'], $this->_settings)) {
+                $result[] = array('label' => $_result['Variable_name'], 'value' => $_result['Value']);
+            }
+        }
+
+        return $result;
     }
 
 }
