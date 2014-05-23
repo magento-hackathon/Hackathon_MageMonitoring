3c4e2c88ecd13eff005197a6b6b7ab49d9725ffd
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2014-05-23 18:51:44 +0200

    Refactored php tab contents as widgets.

---
+++ app/code/community/Hackathon/MageMonitoring/Model/Widget/System/PhpInfo.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Hackathon
+ * @package     Hackathon_MageMonitoring
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Hackathon_MageMonitoring_Model_Widget_System_PhpInfo extends Hackathon_MageMonitoring_Model_Widget_System_Abstract
+                                                           implements Hackathon_MageMonitoring_Model_Widget
+{
+    /**
+     * (non-PHPdoc)
+     * @see Hackathon_MageMonitoring_Model_Widget::getName()
+     */
+    public function getName()
+    {
+        return 'PHP Information';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Hackathon_MageMonitoring_Model_Widget::getVersion()
+     */
+    public function getVersion()
+    {
+        return '1.0';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Hackathon_MageMonitoring_Model_Widget::getOutput()
+     */
+    public function getOutput()
+    {
+        $block = $this->newMonitoringBlock();
+
+        $info = Mage::helper('magemonitoring')->getPhpInfoArray();
+        foreach ($info as $title => $configs) {
+            $block->addHeaderRow($title);
+            if (is_array($configs)) {
+                foreach ($configs as $key => $config) {
+                    $block->addRow('info', $key, implode(' (new) - ', (array) $config));
+                }
+            }
+        }
+
+        $this->_output[] = $block;
+        return $this->_output;
+    }
+
+}
