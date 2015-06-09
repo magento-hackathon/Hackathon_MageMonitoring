1db5006c03814a82f4ec8c901ef1f03300940e1d
Sylvain Rayé <sly@diglin.com>
2015-06-10 01:20:05 +0200

    Replace "general" default mail to with "support" to prevent sending email to shop owner without he understand why he receives such emails

--- app/code/community/Hackathon/MageMonitoring/Model/Widget/Watchdog.php
+++ app/code/community/Hackathon/MageMonitoring/Model/Widget/Watchdog.php
@@ -44,7 +44,7 @@ class Hackathon_MageMonitoring_Model_Widget_Watchdog extends Hackathon_MageMonit
     protected $_defWatchdogCron = '*/5 * * * *';
     protected $_defWatchdogMailto = null;
 
-    const DEFAULT_MAILTO = 'general';
+    const DEFAULT_MAILTO = 'support';
     const DEFAULT_DISABLED = 1;
 
     protected $_defaultConfig = true;
